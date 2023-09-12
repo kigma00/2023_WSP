59a216f0c373e4537cdbd5363b4fed3c2e2fdf8b
G0r4ni8 <76733807+kigma00@users.noreply.github.com>
2023-09-12 20:28:39 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 // 객체 현재 동잗하는 html
-document.getElementById('time').innerHTML = new Date().toLocaleDateString()
+document.getElementById('time').innerHTML = new Date().toLocaleString()
+
+function pink() {
+  document.body.style.backgroundColor = 'pink'
+}
+
+function lightblue() {
+  document.body.style.backgroundColor = 'lightblue'
+}
+
+function yellow() {
+  document.body.style.backgroundColor = 'yellow'
+}
+
+function reset() {
+  document.body.style.backgroundColor = 'linen'
+}
